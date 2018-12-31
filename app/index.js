970c25261b96082bd560280a63aa515f2977cd04
Adi Roiban <adi.roiban@chevah.com>
2018-12-31 01:10:56 +0000

    Use a single virtual list. Update colors

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 import document from "document";
 import * as messaging from "messaging";
 
+// Store an in memory cache of the configuration.
+let settings = {
+  'things_on_off': [],
+  'things_dimmable': []
+}
+
 // Message is received from the companion
 messaging.peerSocket.onmessage = event => {
   //console.log(`App received: ${JSON.stringify(event)}`);
@@ -27,8 +33,6 @@ function send(data) {
 
 // ------------- Configuration handling -----------------
 
-// Store an in memory cache of the configuration.
-let settings = {};
 
 // A new change was made to the configuration.
 function handleConfiguration(data) {
@@ -37,7 +41,7 @@ function handleConfiguration(data) {
     // Not a configuration event.
     return
   }
-    
+
   if (data.value) {
     // Start by setting the raw value.
     settings[data.key] = data.value;
@@ -50,7 +54,6 @@ function handleConfiguration(data) {
   if (data.key === 'things_dimmable') {
     updateThingsDimmable()
   }
-  
 }
 
 // ---------- UI handling --------------
@@ -58,7 +61,6 @@ function handleConfiguration(data) {
 
 
 let virtualOnOffList = document.getElementById("things-on-off-list")
-let virtualDimmableList = document.getElementById("things-dimmable-list")
 
 virtualOnOffList.delegate = {
   getTileInfo: function(index) {
@@ -70,79 +72,70 @@ virtualOnOffList.delegate = {
   },
   configureTile: function(tile, info) {
     if (info.type == "my-pool") {
-      // Only let the marquee run for a few seconds.
-      // And try to only 
-      let marquee = tile.getElementById("marquee");
-      let thing_name = settings['things_on_off'][info.index].name;
-      marquee.text = thing_name;
-      setTimeout(() => marquee.state = "enabled", 1000);
-      setTimeout(() => marquee.state = "disabled", (330 * thing_name.length) + 1000);
-      
-      let on_button = tile.getElementById("button-on");
-      on_button.onclick = evt => {
-        console.log(`ON: ${info.index}`);
-        send({'type': 'switch', 'key': thing_name, 'value': 'on'})
-      };
-      let off_button = tile.getElementById("button-off");
-      off_button.onclick = evt => {
-        console.log(`OFF: ${info.index}`);
-        send({'type': 'switch', 'key': thing_name, 'value': 'off'})
-      };
+      let data
+      let on_off = true
+      if (info.index < settings['things_on_off'].length) {
+        data = settings['things_on_off'][info.index]
+      } else {
+        data = settings['things_dimmable'][info.index - settings['things_on_off'].length]
+        on_off = false
+      }
 
-    }
-  }
-};
-
-virtualDimmableList.delegate = {
-  getTileInfo: function(index) {
-    return {
-      type: "my-pool",
-      value: "Menu item",
-      index: index
-    };
-  },
-  configureTile: function(tile, info) {
-    if (info.type == "my-pool") {
       // Only let the marquee run for a few seconds.
-      // And try to only 
+      // And try to only
       let marquee = tile.getElementById("marquee");
-      let thing_name = settings['things_dimmable'][info.index].name;
+      let thing_name = data.name;
       marquee.text = thing_name;
       setTimeout(() => marquee.state = "enabled", 1000);
       setTimeout(() => marquee.state = "disabled", (330 * thing_name.length) + 1000);
 
-      //set [light name] to 50%
-      let dim_0 = tile.getElementById("dim-0");
-      dim_0.onclick = evt => {
-        console.log(`OFF: ${info.index}`);
-        send({'type': 'switch', 'key': thing_name, 'value': 'off'})
-      };
+      if (on_off) {
+        tile.getElementById('buttons-dim').style.display = 'none'
+        tile.getElementById('buttons-on-off').style.display = 'inline'
+        let on_button = tile.getElementById("button-on");
+        on_button.onclick = evt => {
+          console.log(`ON: ${thing_name}`);
+          send({'type': 'switch', 'key': thing_name, 'value': 'on'})
+        };
+        let off_button = tile.getElementById("button-off");
+        off_button.onclick = evt => {
+          console.log(`OFF: ${thing_name}`);
+          send({'type': 'switch', 'key': thing_name, 'value': 'off'})
+        };
+      } else {
+        tile.getElementById('buttons-dim').style.display = 'inline'
+        tile.getElementById('buttons-on-off').style.display = 'none'
+        //set [light name] to 50%
+        let dim_0 = tile.getElementById("dim-0");
+        dim_0.onclick = evt => {
+          console.log(`Dim 0%: ${thing_name}`);
+          send({'type': 'switch', 'key': thing_name, 'value': 'off'})
+        };
 
-      let dim_1 = tile.getElementById("dim-1");
-      dim_1.onclick = evt => {
-        console.log(`Dim 25%: ${info.index}`);
-        send({'type': 'set_brightness', 'key': thing_name, 'value': '25'})
-      };
-      
-      let dim_2 = tile.getElementById("dim-2");
-      dim_2.onclick = evt => {
-        console.log(`Dim 50%: ${info.index}`);
-        send({'type': 'set_brightness', 'key': thing_name, 'value': '50'})
-      };
+        let dim_1 = tile.getElementById("dim-1");
+        dim_1.onclick = evt => {
+          console.log(`Dim 25%: ${thing_name}`);
+          send({'type': 'set_brightness', 'key': thing_name, 'value': '25'})
+        };
 
-      let dim_3 = tile.getElementById("dim-3");
-      dim_3.onclick = evt => {
-        console.log(`Dim 75%: ${info.index}`);
-        send({'type': 'set_brightness', 'key': thing_name, 'value': '75'})
-      };
-      
-      let dim_4 = tile.getElementById("dim-4");
-      dim_4.onclick = evt => {
-        console.log(`ON: ${info.index}`);
-        send({'type': 'set_brightness', 'key': thing_name, 'value': '100'})
-      };
+        let dim_2 = tile.getElementById("dim-2");
+        dim_2.onclick = evt => {
+          console.log(`Dim 50%: ${thing_name}`);
+          send({'type': 'set_brightness', 'key': thing_name, 'value': '50'})
+        };
 
-      
+        let dim_3 = tile.getElementById("dim-3");
+        dim_3.onclick = evt => {
+          console.log(`Dim 75%: ${thing_name}`);
+          send({'type': 'set_brightness', 'key': thing_name, 'value': '75'})
+        };
+
+        let dim_4 = tile.getElementById("dim-4");
+        dim_4.onclick = evt => {
+          console.log(`ON: ${thing_name}`);
+          send({'type': 'set_brightness', 'key': thing_name, 'value': '100'})
+        };
+      }
     }
   }
 };
@@ -155,7 +148,8 @@ function updateThingsOnOff() {
   console.log('Got on/off things:' + raw_data);
   settings['things_on_off'] = JSON.parse(raw_data);
   // `length` must be set AFTER `delegate`.
-  virtualOnOffList.length = settings['things_on_off'].length;
+  virtualOnOffList.length = settings['things_on_off'].length + settings['things_dimmable'].length
+
 }
 
 // Update the list of things that can be only be turned on/off
@@ -164,5 +158,5 @@ function updateThingsDimmable() {
   console.log('Got dimmable things:' + raw_data);
   settings['things_dimmable'] = JSON.parse(raw_data);
   // `length` must be set AFTER `delegate`.
-  virtualDimmableList.length = settings['things_dimmable'].length;
+  virtualOnOffList.length = settings['things_on_off'].length + settings['things_dimmable'].length
 }
